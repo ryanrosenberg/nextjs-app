3448cb0d43676e7d049b0848120aaafe089881be
ryanrosenberg <ryan.rosenberg@outlook.com>
2023-05-15 18:32:54 -0700

    release-ready about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,19 +1,10 @@
+import styles from "./about.module.css"
+
 export default function About() {
   
   return (
     <>
       <div>
-        <h1 className="page-title">About</h1>
-        <h2>To-do</h2>
-        <ul>
-          <h4>Pre-release</h4>
-          <li>put everything on Firebase</li>
-          <li>get feedback</li>
-          <h4>Post-release</h4>
-          <li>Circuit records</li>
-          <li>player best games?</li>
-          <li>lots of data cleaning I'm sure</li>
-        </ul>
         <p>
           Welcome to College Quizbowl Stats. This site has two intended
           purposes:
@@ -35,18 +26,27 @@ export default function About() {
             easy to access and use.
           </li>
         </ul>
-        <h2>Status</h2>
-        <p>
-          Currently, the site includes all tournaments in the seasons from
-          2011-2012 to 2021-2022 that have posted stats, with the exception of
+        <h3 className={styles.header}>Status</h3>
+        <p className={styles.paragraph}>
+          Currently, the site includes all closed collegiate tournaments in the seasons from 2011-2012 to 2021-2022 that have posted stats, with the exception of
           those that only posted stats on Neg5 or that posted stats without a
-          Scoreboard page. The below table summarizes the status of each
-          competition year:
+          Scoreboard page. Open tournaments that were primarily composed of closed collegiate teams, as well as Chicago Open, were also included.
         </p>
-        <h2>Set Difficulty</h2>
-        <p>
+        <h3 className={styles.header}>Data corrections and name changes</h3>
+        <p className={styles.paragraph}>
+          If you have any name changes or data corrections (e.g. two player entries who are actually the same player and should be combined, a team that's mislabeled) please email me at <a href="mailto:ryanr345@gmail.com">ryanr345@gmail.com</a> and I'll fix it ASAP.
+        </p>
+        <h3 className={styles.header}>Set difficulty</h3>
+        <p className={styles.paragraph}>
           This site uses the same set difficulty notation as{" "}
           <a href="collegequizbowlcalendar.com">collegequizbowlcalendar.com</a>.
+        </p>
+        <h3 className={styles.header}>Future plans</h3>
+        <p className={styles.paragraph}>
+          An immediate priority is streamlining data ingestion so more tournaments can be added (both future tournaments and filling in more past tournaments). Eventually, I'd like the site to at least host stats from all available closed collegiate tournaments, and perhaps all open tournaments.
+          <br></br>
+          <br></br>
+          This site is designed to be extensible: it can easily be configured to host additional data, such as detailed stats or player/team strength metrics. If you'd like to work on adding these, please contact me at <a href="mailto:ryanr345@gmail.com">ryanr345@gmail.com</a>.
         </p>
       </div>
     </>
