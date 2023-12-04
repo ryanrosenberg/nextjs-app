212d1c223e7f2b7c65d4dd0141d3a7c197eab00e
ryanrosenberg <ryan.rosenberg@outlook.com>
2023-12-03 17:52:01 -0800

    update to next 14

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({
-  // your Next.js configuration
-  experimental: {
-    appDir: true,
-  },
-})
