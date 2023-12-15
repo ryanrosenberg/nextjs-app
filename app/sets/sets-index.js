fd7ccf862b26e06c7b10feb0992570ab75001e06
ryanrosenberg <ryan.rosenberg@outlook.com>
2023-12-15 12:53:11 -0500

    update editor table to use postgres

--- app/sets/sets-index.js
+++ app/sets/sets-index.js
@@ -1,14 +1,36 @@
-'use client'
+'use client';
 
 import EditorTable from "../../components/editor_table";
+import _ from "lodash";
 
 export default function SetsIndex({ result }) {
   const data = result.props.result;
+
+  data.map((item) => {
+    switch (item.difficulty) {
+      case 1:
+        item.difficulty = "<div class = 'diffdots easy-diff'>&#x25CF;</div>";
+        break;
+      case 2:
+        item.difficulty =
+          "<div class = 'diffdots medium-diff'>&#x25CF;&#x25CF;</div>";
+        break;
+      case 3:
+        item.difficulty =
+          "<div class = 'diffdots regionals-diff'>&#x25CF;&#x25CF;&#x25CF;</div>";
+        break;
+      case 4:
+        item.difficulty =
+          "<div class = 'diffdots nationals-diff'>&#x25CF;&#x25CF;&#x25CF;&#x25CF;</div>";
+        break;
+    }
+    return item;
+  });
   return (
     <>
       <div>
         <h1 className="page-title">Sets</h1>
-        <EditorTable data = {data} />
+        <EditorTable data={data} />
       </div>
     </>
   );
