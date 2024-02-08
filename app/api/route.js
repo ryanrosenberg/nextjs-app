35f7ba310fe9842381fc04ad7bac89cb0b7f462c
ryanrosenberg <ryan.rosenberg@outlook.com>
2024-02-07 19:34:47 -0800

    actual api

--- app/api/route.js
+++ app/api/route.js
@@ -3,4 +3,5 @@ import { revalidatePath } from 'next/cache'
 export const dynamic = 'force-dynamic' // defaults to auto
 export async function GET(request) {
     revalidatePath('/tournaments/1987')
+    return(Response('Path revalidated'))
 }
